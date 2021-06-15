659e4b39869652bfa24a6b4f538a0d270f7907af
Xm798 <i@rneko.com>
2021-06-15 15:12:26 +0800

    init

---
+++ QuanX/Emby/README.md
@@ -0,0 +1,5 @@
+## Emby Unlock
+```
+[rewrite_remote]
+https://raw.githubusercontent.com/exsense/cookies/main/Rewrite/emby.qxrewrite, tag=Emby Premiere, update-interval=86400, enabled=true
+```
---
+++ QuanX/Emby/emby-YukiCoco.js
@@ -0,0 +1,39 @@
+// https://cdn.jsdelivr.net/gh/YukiCoco/YukiUsefulTools@YukiCoco-patch-1/unlock_emby.js
+
+let body = $response.body;
+if($request.url.includes("/admin/service/appstore/register"))
+{
+    body = 
+    `
+    {"featId": "","registered": true,"expDate": "2099-01-01","key": ""}
+    `
+} else if($request.url.includes("/emby/Plugins/SecurityInfo"))
+{
+    body = 
+    `
+    {SupporterKey: "", IsMBSupporter: true}
+    `
+} else if($request.url.includes("/admin/service/registration/validateDevice"))
+{
+    body = 
+    `
+    {"cacheExpirationDays": 365,"message": "Device Valid","resultCode": "GOOD"}
+    `
+} else if($request.url.includes("/admin/service/registration/validate"))
+{
+    body = 
+    `
+    {"featId":"","registered":true,"expDate":"2099-01-01","key":""}
+    `
+}else if($request.url.includes("/admin/service/registration/getStatus"))
+{
+    body = 
+    `
+    {"deviceStatus":"0","planType":"Lifetime","subscriptions":{}}
+    `
+}
+
+$done({
+    status : 200,
+    body : body
+});
